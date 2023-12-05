cba50219ad7dae3a922289cd8de1359eace79685
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2023-12-05 13:46:44 +0100

    bugfix: key in map

--- src/app/page.js
+++ src/app/page.js
@@ -106,6 +106,7 @@ export default function Home () {
           <ol className='bg-[#ececf3] text-slate-500 h-fit max-h-[180px] min-w-[200px] list-decimal space-y-1 divide-y-2 divide-slate-200 py-4 px-8 w-fit'>
             {data?.map((song, index) => (
               <li
+                key={index}
                 className='hover:bg-slate-300'
                 onClick={() => setCurrentSong(index)}
               >
